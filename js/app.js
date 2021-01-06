1a846adf93f18086b1c6cb3912269beea6ef522a
germanmend <germendg@gmail.com>
2021-01-05 22:16:38 -0600

    added log

--- js/app.js
+++ js/app.js
@@ -73,7 +73,7 @@ var App = function () {
 			callback(config);
 		} catch (e) {
 			if (e.code === "ENOENT") {
-				Log.error(Utils.colors.error("WARNING! Could not find config file. Please create one. Starting with default configuration."));
+				Log.error(Utils.colors.error("WARNING! Could not find config file. Please create one. Starting with default configuration: " + configFilename));
 			} else if (e instanceof ReferenceError || e instanceof SyntaxError) {
 				Log.error(Utils.colors.error("WARNING! Could not validate config file. Starting with default configuration. Please correct syntax errors at or above this line: " + e.stack));
 			} else {
