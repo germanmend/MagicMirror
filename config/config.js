2211d714340a4632c7a6f988dc68e504ca99df88
germanmend <germendg@gmail.com>
2021-01-06 19:40:09 -0600

    added spotify

--- config/config.js
+++ config/config.js
@@ -37,21 +37,6 @@ var config = {
       }
     },
     {
-  		module: "MMM-OpenmapWeather",
-  		position: "top_right",	// This can be any of the regions.
-  									// Best results in left or right regions.
-  		config: {
-    			// See 'Configuration options' for more information.
-    			locationID: "3530597", //Location ID from http://openweathermap.org/help/city_list.txt
-    			appid: "a9aa81340342e664ca7140e66f7c7d3e",  //openweathermap.org API key
-    			colorIcon: true,
-          degreeLabel: true,
-          updateInterval: 1800000,
-          useKMPHWind: true,
-          roundTemp: true
-  		}
-    },
-    {
         module: 'MMM-CoinMarketCap',
         position: "top_left",
         header: "Cryptos",
@@ -63,6 +48,18 @@ var config = {
             // See below for more Configuration Options
         }
       },
+      {
+  module: "MMM-NowPlayingOnSpotify",
+  position: "top_right",
+
+  config: {
+    clientID: "854da5fa52af4b7cba0ce0a87a277840",
+    clientSecret: "5499cd55b2ce442891e24846ac6dc7e5",
+    accessToken: "BQByq8Huix9vJr-4svAbEiXxOtt7n9qP_epBJcC_6ProPjAP6BtLUSGNhEQ4LS1lQzP2ASshVMTgPk4TwyUqNygs-_fDQEWF32sLF1ZO_iArOljYhgazZWIVXUGS4oSylivbQ87T7KDRuCQGPPAWeSw",
+    refreshToken: "AQBUGjNxvpZY6ejw7qjdVUJ_vT8k0YT2wh4JTw_c65dnOkKc9YSHF8PbfmZVqVzh4nHhAkkgySdiXqBwvVKdhsZzF2p_MhS5W5qgxJ-HMZvRkuptgLDsSLgDdZajMbWVFMY",
+    moduleWidth: "200"
+  }
+},
     {
       module: "calendar",
       header: "US Holidays",
@@ -72,13 +69,19 @@ var config = {
           {
             symbol: "calendar-alt",
             url: "webcal://www.calendarlabs.com/ical-calendar/ics/60/MX_Holidays.ics"
-            //url: "webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics"
-            //www.calendarlabs.com/ical-calendar/ics/75/MX_Holidays.ics
           }
         ],
         maximumEntries: 5
       }
     },
+    {
+			module: "compliments",
+			position: "bottom_bar",
+      config: {
+        remoteFile: 'https://github.com/germanmend/MagicMirror/blob/master/config/compliments.json',
+        updateInterval: 5000,
+      }
+		},
     {
       module: "newsfeed",
       position: "bottom_bar",
